f55a334c6321dce0d748840e6b4a582690dc8c8c
Bolu <aprilweather14@gmail.com>
2022-09-09 13:47:52 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: finally
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int man;
+
+	for (man = 48; man < 58; man++)
+	{
+		putchar(man);
+		if (man != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
