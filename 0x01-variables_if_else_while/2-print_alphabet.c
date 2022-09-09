310d74b00cb45e75a00456cee9667a75473477ac
Bolu <aprilweather14@gmail.com>
2022-09-09 13:02:07 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: another code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char sam;
+
+	for (sam = 'a'; sam <= 'z'; sam++)
+	{
+		putchar(sam);
+	}
+	putchar('\n');
+	return (0);
+}
