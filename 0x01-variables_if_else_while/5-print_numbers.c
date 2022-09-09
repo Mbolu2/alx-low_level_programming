c2d8fbfa51d2bf0f7741a345935882e5eea034b3
Bolu <aprilweather14@gmail.com>
2022-09-09 13:23:53 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Hvae mercy Lord Jesus
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int bolu;
+
+	for (bolu = 0; bolu < 10; bolu++)
+	{
+		printf("%d", bolu);
+	}
+	printf("\n");
+	return (0);
+}
