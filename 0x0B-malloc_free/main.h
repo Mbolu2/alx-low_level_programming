43bb50135742829cec59a1fe23a616ba46a45190
Bolu <aprilweather14@gmail.com>
2022-10-04 19:49:04 -0700

    my first commit

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,8 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 int _putchar(char c);
-char *create_array(unsigend int size, cahr c);
-char *strdup(char *str);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
